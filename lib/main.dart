38daef85e210cffebbfb105940a4cbc1d363ddc9
maidilfattah <maidilfattah@gmail.com>
2026-05-28 21:56:52 +0700

    mas aku latihan lagi layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/latihanlayout.dart';
 //import 'package:flutter_application_1/latihancrud.dart';
-import 'package:flutter_application_1/latihanlocals.dart';
-import 'package:flutter_application_1/latihanlocals2.dart';
+// import 'package:flutter_application_1/latihanlocals.dart';
+// import 'package:flutter_application_1/latihanlocals2.dart';
 
 //import 'package:flutter_application_1/latihanlocals.dart';
 void main() {
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: LatihanlocalsScreen2(),
+      home: LatihanLayout(),
     );
   }
 }
