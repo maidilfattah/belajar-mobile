9d9c806bdcf6b252644f4307da3dc78a0200494c
maidilfattah <maidilfattah@gmail.com>
2026-06-09 15:50:44 +0700

    latihan container

--- lib/latihantoday.dart
+++ lib/latihantoday.dart
@@ -11,14 +11,18 @@ class _LatihanTodayState extends State<LatihanToday> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Latihan row dan coloum")),
+      appBar: AppBar(
+        title: Text("Row and Column", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.green,
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
+        children: [
           Text("Text 1"),
-          Text("Text 2"),
-          Row(children: <Widget>[Text("Text 3 "), Text("Text 4")]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text("Text 2")],
+          ),
         ],
       ),
     );
