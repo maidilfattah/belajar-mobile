dae8c13dbefcab92ff21fe627a5131aa6eddb52a
maidilfattah <maidilfattah@gmail.com>
2026-06-13 18:48:19 +0700

    up latihan animationContainer

--- lib/latihantoday.dart
+++ lib/latihantoday.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class LatihanToday extends StatefulWidget {
@@ -8,25 +9,48 @@ class LatihanToday extends StatefulWidget {
 }
 
 class _LatihanTodayState extends State<LatihanToday> {
+  final Random _random = Random();
+
+  // 1. Define state variables with initial values
+  Color _color = Colors.blue;
+  double _width = 100.0;
+  double _height = 100.0;
+
+  // 2. Create a method to randomize the values
+  void _randomizeContainer() {
+    setState(() {
+      _color = Color.fromARGB(
+        255,
+        _random.nextInt(256),
+        _random.nextInt(256),
+        _random.nextInt(256),
+      );
+      _width = 50.0 + _random.nextInt(101);
+      _height = 50.0 + _random.nextInt(101);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Latihan TextStyle")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Hallo Guys",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                decoration: TextDecoration.underline,
-                decorationColor: Colors.red,
-                decorationStyle: TextDecorationStyle.wavy,
-              ),
-            ),
-          ],
+      appBar: AppBar(
+        title: const Text(
+          "Latihan AnimationContainer",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: GestureDetector(
+        onTap: _randomizeContainer, // 3. Trigger randomization on tap
+        child: Center(
+          child: AnimatedContainer(
+            duration: const Duration(seconds: 1),
+            curve: Curves
+                .fastOutSlowIn, // Optional: Makes the animation look smoother
+            color: _color,
+            width: _width,
+            height: _height,
+          ),
         ),
       ),
     );
