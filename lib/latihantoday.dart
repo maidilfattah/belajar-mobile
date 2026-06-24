972a9bd1b9877d3ce484f19f445b87ffa4accba5
maidilfattah <maidilfattah@gmail.com>
2026-06-24 12:45:48 +0700

    latihan appbar example lagi

--- lib/latihantoday.dart
+++ lib/latihantoday.dart
@@ -12,53 +12,20 @@ class _LatihanTodayState extends State<LatihanToday> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Halaman 1"),
-        backgroundColor: Colors.brown,
-        foregroundColor: Colors.white,
-      ),
-      body: Center(
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.brown,
-            foregroundColor: Colors.white,
-            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 25),
+        leading: Icon(Icons.adb, color: Colors.black),
+        title: Text("AppBar Example", style: TextStyle(color: Colors.black)),
+        actions: <Widget>[
+          IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.exit_to_app)),
+        ],
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.red, Colors.pink],
+              begin: FractionalOffset.topLeft,
+              end: FractionalOffset.bottomRight,
+            ),
           ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => HalamanKedua()),
-            );
-          },
-          child: Text("Halaman 2", style: TextStyle(fontSize: 16)),
-        ),
-      ),
-    );
-  }
-}
-
-class HalamanKedua extends StatelessWidget {
-  const HalamanKedua({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Halaman 2"),
-        backgroundColor: Colors.brown,
-        foregroundColor: Colors.white,
-        automaticallyImplyLeading: false,
-      ),
-      body: Center(
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.brown,
-            foregroundColor: Colors.white,
-            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 25),
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text("Halaman 1", style: TextStyle(fontSize: 16)),
         ),
       ),
     );
