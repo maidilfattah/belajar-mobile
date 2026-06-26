e0e9f61524631c3cf90a1f3306ffdead68f63dc1
maidilfattah <maidilfattah@gmail.com>
2026-06-26 19:47:54 +0700

    latihan login

--- lib/latihantoday.dart
+++ lib/latihantoday.dart
@@ -11,34 +11,33 @@ class _LatihanTodayState extends State<LatihanToday> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login")),
+      appBar: AppBar(
+        title: Text("Login", style: TextStyle(fontWeight: FontWeight.bold)),
+      ),
       body: Padding(
         padding: EdgeInsets.all(24),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Silahkan Login"),
+            Text("Silahkan Login", style: TextStyle(fontSize: 18)),
             const SizedBox(height: 30),
             TextField(
               keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                labelText: 'Email',
+              decoration: InputDecoration(
+                labelText: "Email",
                 border: OutlineInputBorder(),
               ),
             ),
             const SizedBox(height: 16),
             TextField(
               obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Password',
+              decoration: InputDecoration(
+                labelText: "Password",
                 border: OutlineInputBorder(),
               ),
             ),
             const SizedBox(height: 16),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(onPressed: null, child: Text('Masuk')),
-            ),
+            ElevatedButton(onPressed: null, child: Text("Masuk")),
           ],
         ),
       ),
