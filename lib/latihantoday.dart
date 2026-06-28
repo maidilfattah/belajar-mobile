707fd4cc12cbfebf3e0966953fa0188e3fb91db6
maidilfattah <maidilfattah@gmail.com>
2026-06-28 12:09:37 +0700

    up latihan login

--- lib/latihantoday.dart
+++ lib/latihantoday.dart
@@ -10,35 +10,95 @@ class LatihanToday extends StatefulWidget {
 class _LatihanTodayState extends State<LatihanToday> {
   @override
   Widget build(BuildContext context) {
+    //ambil data input
+    final TextEditingController _emailController = TextEditingController();
+    final TextEditingController _passwordController = TextEditingController();
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login", style: TextStyle(fontWeight: FontWeight.bold)),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Silahkan Login", style: TextStyle(fontSize: 18)),
-            const SizedBox(height: 30),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: "Email",
-                border: OutlineInputBorder(),
-              ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.white, Colors.deepPurple],
+            end: Alignment.topLeft,
+            begin: Alignment.bottomRight,
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsetsGeometry.symmetric(horizontal: 28),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  "Silahkan Login",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 28),
+
+                TextField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    labelStyle: TextStyle(color: Colors.blueGrey),
+                    prefixIcon: Icon(
+                      Icons.email_outlined,
+                      color: Colors.purple,
+                    ),
+                    filled: true,
+                    fillColor: const Color(0xFF1E1E1E),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide(color: Colors.white, width: 1.5),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    labelStyle: TextStyle(color: Colors.blueGrey),
+                    prefixIcon: Icon(
+                      Icons.password_outlined,
+                      color: Colors.purple,
+                    ),
+                    filled: true,
+                    fillColor: const Color(0xFF1E1E1E),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide(color: Colors.white, width: 1.5),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 28),
+
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.deepPurple,
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(16),
+                    ),
+                    elevation: 5,
+                  ),
+                  child: Text(
+                    "LOGIN",
+                    style: TextStyle(fontSize: 16, letterSpacing: 1.5),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 16),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Password",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(onPressed: null, child: Text("Masuk")),
-          ],
+          ),
         ),
       ),
     );
